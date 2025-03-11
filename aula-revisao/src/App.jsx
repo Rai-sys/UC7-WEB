068c8475d3f10e69240020a32204ed33f72240c0
RAILANE SIQUEIRA SERAFIM <a95367729@rn.senac.local>
2025-03-11 15:31:06 -0300

    add: revisao

---
+++ aula-revisao/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Button from './components/Button/Button'
+
+function App() {
+
+  return (
+    <div>
+      <h1>Olá, Mundo 🐥</h1>
+      <p>Componente react</p>
+      <Button />
+    </div>
+  )
+}
+
+export default App;
