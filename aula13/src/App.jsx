4294998ce36ef11b378ee1905a6a8b197f05d7ff
RAILANE SIQUEIRA SERAFIM <a95367729@rn.senac.local>
2025-03-25 17:14:28 -0300

    add: adicionado conteudo da aula finalizado

---
+++ aula13/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import ButtonTemaMode from './components/Button/ButtonTemaMode'
+
+function App() {
+
+  return (
+    <>
+    <ButtonTemaMode />
+    </>
+  )
+}
+
+export default App
