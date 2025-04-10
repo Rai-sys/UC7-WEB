17959543134f3866cc5efdcafaeaabc52522b476
RAILANE SIQUEIRA SERAFIM <a95367729@rn.senac.local>
2025-04-10 17:34:29 -0300

    add: codigo do front

--- aula11/atividade/src/components/Main/Main.jsx
+++ aula11/atividade/src/components/Main/Main.jsx
@@ -1,29 +1,7 @@
 import React from "react";
 import style from "./Main.module.css";
 import Band from "../Band/Band";
-
-const bandas = [
-    {
-        nome:'Banda 01',
-        imagem: 'https://cdn.ontourmedia.io/gunsnroses/site_v2/animations/gnr_loop_logo_01.jpg',
-        descricao: 'Descrição da Banda 1. Lorem ipsum dolor sit amet, consectetur adipiscing elit'
-    },
-    {
-        nome:'Banda 02',
-        imagem: 'https://cdn.ontourmedia.io/gunsnroses/site_v2/animations/gnr_loop_logo_01.jpg',
-        descricao: 'Descrição da Banda 2. Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
-    },
-    {
-        nome:'Banda 03',
-        imagem: 'https://cdn.ontourmedia.io/gunsnroses/site_v2/animations/gnr_loop_logo_01.jpg',
-        descricao: 'Descrição da Banda 3. Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
-    },
-    {
-        nome:'Banda 04',
-        imagem: 'https://cdn.ontourmedia.io/gunsnroses/site_v2/animations/gnr_loop_logo_01.jpg',
-        descricao: 'Descrição da Banda 4. Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
-    }
-];
+import bandas from "../../data/bancoBands";
 
 function Main() {
     return (
