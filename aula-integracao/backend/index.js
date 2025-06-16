74a75469f6be166eeaeda779001571198916df97
Rai-sys <railaneserafim484@gmail.com>
2025-06-16 02:22:54 -0300

    fix: modificado conexao do banco

--- aula-integracao/backend/index.js
+++ aula-integracao/backend/index.js
@@ -16,9 +16,25 @@ app.use('/cursos', cursoRoutes);
 app.use('/alunos', alunoRoutes);
 
 
+sequelize.sync().then(async () => {
+    try {
+        await sequelize.authenticate();
+        console.log('Conexão com o banco de dados estabelecida com sucesso.');
 
-sequelize.sync().then(() => {
-    app.listen(port, () => {
-        console.log(`Servidor rodando na porta ${port}`);
-    })
+        app.listen(port, () => {
+            console.log(`Servidor rodando na porta ${port}`);
+        });
+    } catch (error) {
+        console.error('Não foi possível conectar ao banco de dados:', error);
+    }
 })
+
+.catch((syncError) => {
+    console.error('Erro ao sincronizar com o banco de dados:', syncError);
+});
+
+// sequelize.sync().then(() => {
+//     app.listen(port, () => {
+//         console.log(`Servidor rodando na porta ${port}`);
+//     })
+// })
